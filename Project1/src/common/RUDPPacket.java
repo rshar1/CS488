1294b0e5f6a847e0d688bbccb0d942e1cc4ca2d8
Raami Sharif <raamisharif@gmail.com>
2019-02-18 23:41:08 -0500

    added some initial files and the CircularQueue implemented

---
+++ Project1/src/common/RUDPPacket.java
@@ -0,0 +1,96 @@
+package common;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * The RUDPPacket contains the headers and payload for a reliable datagram. This packet class
+ * also has some methods to easily transfer the packet to byte format and vice versa so as to send
+ * it through the socket.
+ * The receiving end, can also convert the incoming packet back into an RUDPPacket and perform
+ * the necessary checks to ensure reliability.
+ */
+public class RUDPPacket {
+
+  private int sequenceNumber; // 4 bytes
+  private int ackNum;         // 4 bytes
+
+  private boolean connectAttempt; // 1 byte
+  private byte[] data;            // 900 bytes max
+
+  public RUDPPacket(int sequenceNumber, int ackNum) {
+    this.sequenceNumber = sequenceNumber;
+    this.ackNum = ackNum;
+  }
+
+  public int getSequenceNumber() {
+    return sequenceNumber;
+  }
+
+  public int getAckNum() {
+    return ackNum;
+  }
+
+  public boolean isConnectAttempt() {
+    return connectAttempt;
+  }
+
+  public void setConnectAttempt(boolean connectAttempt) {
+    this.connectAttempt = connectAttempt;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public void setData(byte[] data) {
+    this.data = data;
+  }
+
+  public byte[] toBytes() {
+
+    byte[] res = null;
+
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream(905);
+        DataOutputStream dOut = new DataOutputStream(out)) {
+
+      dOut.writeInt(sequenceNumber);
+      dOut.writeInt(ackNum);
+      dOut.writeBoolean(connectAttempt);
+      dOut.write(data);
+
+      res = out.toByteArray();
+
+    } catch (IOException exc) {
+      // todo error converting
+    }
+
+    return res;
+  }
+
+  public static RUDPPacket fromBytes(byte[] bytes) throws IOException {
+
+    RUDPPacket packet = null;
+
+    try (ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        DataInputStream din = new DataInputStream(in)) {
+
+      int sequenceNumber = din.readInt();
+      int ackNum = din.readInt();
+      boolean connectAttempt = din.readBoolean();
+      byte[] data = new byte[900];
+      int dataLength = din.read(data);
+
+      packet = new RUDPPacket(sequenceNumber, ackNum);
+      packet.connectAttempt = connectAttempt;
+      packet.data = data;
+
+    }
+
+    return packet;
+  }
+
+}
---
+++ Project1/src/common/RUDPSocket.java
@@ -0,0 +1,146 @@
+package common;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * A bi-directional communication protocol. This socket is the interface for the application layer
+ * to send and receive data reliably to another end-system also using this protocol.
+ * This class stores the buffer, and ensures that all messages are acked as well as sending it's own
+ * acks.
+ */
+public class RUDPSocket implements AutoCloseable {
+
+  enum STATUS {
+    CONNECTED, CONNECTING, DISCONNECTED;
+  }
+
+  private DatagramSocket socket;
+  private int sourcePort;
+  private STATUS status = STATUS.DISCONNECTED;
+
+  private InetAddress remoteAddress;
+  private int remotePort;
+
+  // This contains the packets that were sent out, and are now awaiting an ack
+  private SenderWindow sender;
+
+  // This collects the packets coming in so that the data can be returned to the application
+  private ReceiverWindow receiver;
+
+
+  public RUDPSocket(int sourcePort) throws SocketException {
+    this.sourcePort = sourcePort;
+    this.socket = new DatagramSocket(sourcePort);
+    this.status = STATUS.DISCONNECTED;
+
+    // Make a thread that listens for any packets coming to the socket
+    Runnable receiver = new Runnable() {
+      @Override
+      public void run() {
+        while (true) {
+
+          byte[] buf = new byte[905];
+          DatagramPacket packet = new DatagramPacket(buf, buf.length);
+          try {
+            socket.receive(packet);
+            processPacket(packet);
+          } catch (IOException exc) {
+            System.out.println("Error receiving packing");
+          }
+        }
+      }
+    };
+
+  }
+
+  /**
+   * Begins a new connection with another RUDP socket
+   * @param address the remote address to connect to
+   * @param port the remote port to connect to
+   * @throws IllegalStateException if this socket is already connected to a remote host
+   */
+  public boolean connect(String address, int port) throws UnknownHostException {
+
+    if (status != STATUS.DISCONNECTED) {
+      throw new IllegalStateException("Cannot connect to multiple hosts");
+    }
+
+    this.remoteAddress = InetAddress.getByName(address);
+    this.remotePort = port;
+    beginConnectionRequest();
+
+    if (status != STATUS.CONNECTED) {
+      status = STATUS.DISCONNECTED;
+      return false;
+    }
+
+    return true;
+  }
+
+  public STATUS getStatus() {
+    return status;
+  }
+
+  public boolean isClosed() {
+    return this.socket.isClosed();
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.socket.close();
+  }
+
+  private void processPacket(DatagramPacket packet) {
+
+    RUDPPacket rPacket;
+    try {
+      rPacket = RUDPPacket.fromBytes(packet.getData());
+    } catch (IOException exc) {
+      System.out.println("Not an RUDP packet");
+      return;
+    }
+
+    switch (status) {
+
+      case CONNECTED:
+        // todo process the packet from the remote address
+        break;
+      case CONNECTING:
+        // todo process the packet from  the connecting address
+        System.out.println("Not yet implemented");
+        break;
+      case DISCONNECTED:
+        // todo process this packet from a remote address looking to initiate a connection
+
+
+        break;
+      default:
+        System.out.println("Status undefined");
+        System.exit(-1);
+
+    }
+
+
+  }
+
+  private void beginConnectionRequest() {
+    socket.connect(remoteAddress, remotePort);
+
+    RUDPPacket rudpPacket = new RUDPPacket(1, 0);
+    rudpPacket.setConnectAttempt(true);
+
+    byte[] payload = rudpPacket.toBytes();
+    DatagramPacket packet = new DatagramPacket(payload, payload.length, remoteAddress, remotePort);
+
+    status = STATUS.CONNECTING;
+
+    // Send this to the SenderWindow
+
+  }
+
+}
