251e4e2f7b40c43c264761f8e3264529793ddebf
Edward Gervis <edward.gervis@gmail.com>
2019-02-19 16:59:50 -0500

    receiver, sender

--- Project1/src/receiver/Receiver.java
+++ Project1/src/receiver/Receiver.java
@@ -1,5 +1,42 @@
 package receiver;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import common.RUDPSocket;
+
 public class Receiver {
 
+	
+	final static int ownPort = 8888;
+	final static int targetPort = 7777;
+	static String host = "localhost";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+	        File file = new File("./Resource/copy_1_tcp.jpg");
+	        FileOutputStream fis = new FileOutputStream(file);
+	        
+	        byte[] data = new byte[1024];
+	        
+	        //socket.setSoTimeout(10000);
+	        System.out.println("Receiver: connection built. about to receive.");
+	        
+	        	try(RUDPSocket socket = new RUDPSocket(ownPort);)
+	        	{
+	        		while(socket.getInputStream().read(data) != -1)
+	        		{
+	        			fis.write(data);
+	        		}
+	        	}
+	        	catch(Exception e)
+		        {
+	        		//todo
+		        }
+	        System.out.println("Receiver: finished.");
+	        
+	        fis.close();
+		}
+	
+	
 }
--- Project1/src/sender/Sender.java
+++ Project1/src/sender/Sender.java
@@ -1,5 +1,43 @@
 package sender;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import common.RUDPSocket;
+
 public class Sender {
 
+	
+	final static int ownPort = 8888;
+	final static int targetPort = 7777;
+	static String host = "localhost";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+	        File file = new File("./Resource/copy_1_tcp.jpg");
+	        FileInputStream fis = new FileInputStream(file);
+	        
+	        byte[] data = new byte[1024];
+	        
+	        //socket.setSoTimeout(10000);
+	        System.out.println("Receiver: connection built. about to receive.");
+	        
+	        	try(RUDPSocket socket = new RUDPSocket(ownPort);)
+	        	{
+	        		socket.connect(host, targetPort);
+	        		while(fis.read(data) != -1)
+	        		{
+	        			socket.getOutputStream().write(data);
+	        		}
+	        	}
+	        	catch(Exception e)
+		        {
+	        		//todo
+		        }
+	        System.out.println("Receiver: finished.");
+	        
+	        fis.close();
+		}
+	
+	
 }
