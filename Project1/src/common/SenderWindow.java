766e52adaf52fa97cb586beeae9d488813343149
Edward Gervis <edward.gervis@gmail.com>
2019-02-21 20:11:16 -0800

    window

--- Project1/src/common/SenderWindow.java
+++ Project1/src/common/SenderWindow.java
@@ -1,16 +1,20 @@
 package common;
 
+import java.net.DatagramSocket;
+
+import common.SenderWindow.Frame;
 
 public class SenderWindow {
 
   class Frame {
     private RUDPPacket packet;
     private boolean isAcked = false;
-    private int inc;
+    private int inc = 0;
   }
-
-
-  private CircularBuffer<Frame> bufferQueue;
+  
+  private static final int timeoutNum = 10;
+  private static final int BUFF_SIZE = 12;
+  private CircularBuffer<Frame> bufferQueue = new CircularBuffer<>(BUFF_SIZE);
 
 
   void acceptAck(int num) {
@@ -22,22 +26,39 @@ public class SenderWindow {
 			  
 	  }
   }
-
+  
+  
+  void timeOut(DatagramSocket socket)
+  {
+	  for(Frame f:this.bufferQueue)
+	  {
+		  f.inc++;
+	  }
+  }
+  
   void getAcks() {
-
+	  //?????????
   }
 
   boolean windowAvailable() {
     // todo return true if there is a free window space
-    return false;
+	  if(this.bufferQueue.size() == this.BUFF_SIZE)
+		  return false;
+    return true;
   }
 
   void addPacket(RUDPPacket packet) {
     // todo
+	  Frame f = new Frame();
+	  f.packet = packet;
+	  this.bufferQueue.add(f);
   }
 
   void slideWindow() {
-
+	  while(this.bufferQueue.peek().isAcked)
+	  {
+		  this.bufferQueue.remove();
+	  }
   }
 
 }
