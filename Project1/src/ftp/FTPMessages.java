579a3af51665fc2c79632aa706a9a60c4fd22a4e
Raami Sharif <raamisharif@gmail.com>
2019-03-20 23:34:05 -0400

    slight optimization to write int

--- Project1/src/ftp/FTPMessages.java
+++ Project1/src/ftp/FTPMessages.java
@@ -25,9 +25,13 @@ public class FTPMessages {
 
   public static void writeInt(OutputStream out, int num) throws IOException {
 
+    byte[] bytes = new byte[4];
+    int index = 0;
     for (int i = 24; i >= 0; i -= 8) {
-      out.write(0xFF & (num >>> i));
+      bytes[index++] = (byte) (0xFF & (num >>> i));
     }
+
+    out.write(bytes);
   }
 
 
