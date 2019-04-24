a2ecda5a438f05d589e1eedbae2881182ab2c302
Raami Sharif <raamisharif@gmail.com>
2019-04-23 21:17:14 -0400

    implemented write packet

--- Project3/raw_tcp_socket.c
+++ Project3/raw_tcp_socket.c
@@ -14,9 +14,6 @@
 //TCP header (struct tcphdr) definition
 #include <linux/tcp.h>
 
-//Data to be sent (appended at the end of the TCP header)
-#define DATA "datastring"
-
 const int MSS = 1460;
 const int wscale = 4;
 const double client_bw = 1544000.0
@@ -105,11 +102,124 @@ int send_packet(int socket,
     // TODO implement send packet to construct the packet and send
     // it based to the provided socket
 
+    int bytes_sent;
+    struct tcphdr *tcpHdr;
+
+    //TCP header + data
+    char packet[MSS];
+
+    //Address struct to sendto()
+    struct sockaddr_in addr_in;
+
+    //Pseudo TCP header to calculate the TCP header's checksum
+    struct pseudoTCPPacket pTCPPacket;
+
+    //Pseudo TCP Header + TCP Header + data
+    char *pseudo_packet;
+
+    //Populate address struct
+    addr_in.sin_family = AF_INET;
+    addr_in.sin_port = htons(m_connection->dst_port);
+    addr_in.sin_addr.s_addr = m_connection->dst_addr;
+
+    //Allocate mem for tcp headers and 0
+    memset(packet, 0, MSS);
+
+    tcpHdr = (struct tcphdr *) packet;
+    data = (char *) (packet + sizeof(struct tcphdr));
+    memcpy(data, content, content_length);
+
+    //Populate tcpHdr
+    // TODO WHAT IS THE SOURCE PORT
+    tcpHdr->source = htons(src_port); //16 bit in nbp format of source port
+    tcpHdr->dest = htons(m_connection->dst_port); //16 bit in nbp format of destination port
+    tcpHdr->seq = seq_nbr % (1<<32); //32 bit sequence number, initially set to zero
+    tcpHdr->ack_seq = ack_nbr % (1<<32); //32 bit ack sequence number, depends whether ACK is set or not
+    tcpHdr->doff = 5; //4 bits: 5 x 32-bit words on tcp header
+    tcpHdr->res1 = 0; //4 bits: Not used
+    tcpHdr->cwr = 0; //Congestion control mechanism
+    tcpHdr->ece = 0; //Congestion control mechanism
+    tcpHdr->urg = 0; //Urgent flag
+    tcpHdr->ack = is_ack; //Acknownledge
+    tcpHdr->psh = 0; //Push data immediately
+    tcpHdr->rst = rst; //RST flag
+    tcpHdr->syn = syn; //SYN flag
+    tcpHdr->fin = fin; //Terminates the connection
+    tcpHdr->window = htons(window);//0xFFFF; //16 bit max number of databytes 
+    tcpHdr->check = 0; //16 bit check sum. Can't calculate at this point
+    tcpHdr->urg_ptr = 0; //16 bit indicate the urgent data. Only if URG flag is set
+
+  //Now we can calculate the checksum for the TCP header
+  // TODO WHAT IS THE SRC ADDR??
+  pTCPPacket.srcAddr = src_addr; //32 bit format of source address
+  pTCPPacket.dstAddr = m_connection->dst_addr; //32 bit format of source address
+  pTCPPacket.zero = 0; //8 bit always zero
+  pTCPPacket.protocol = IPPROTO_TCP; //8 bit TCP protocol
+  pTCPPacket.TCP_len = htons(sizeof(struct tcphdr) + content_length); // 16 bit length of TCP header
+
+  //Populate the pseudo packet
+  pseudo_packet = (char *) malloc((int) (sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) + content_length);
+  memset(pseudo_packet, 0, sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) + content_length);
+
+  //Copy pseudo header
+  memcpy(pseudo_packet, (char *) &pTCPPacket, sizeof(struct pseudoTCPPacket));
+ 
+  //Calculate check sum: zero current check, copy TCP header + data to pseudo TCP packet, update check
+  tcpHdr->check = 0;
+
+  //Copy tcp header + data to fake TCP header for checksum
+  memcpy(pseudo_packet + sizeof(struct pseudoTCPPacket), tcpHdr, sizeof(struct tcphdr) + content_length);
+
+  //Set the TCP header's check field
+  tcpHdr->check = (csum((unsigned short *) pseudo_packet, (int) (sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) +  content_length)));
+
+  printf("TCP Checksum: %d\n", (int) tcpHdr->check);
+
+  //Finally, send packet
+  if((bytes_sent = sendto(sock, packet, MSS, 0, (struct sockaddr *) &addr_in, sizeof(addr_in))) < 0) {
+    perror("Error on sendto()");
+  }
+  else {
+    printf("Success! Sent %d bytes.\n", bytes_sent);
+  }
+
+
 }
 
 int read_packet(struct victim_connection *m_victim, int sock) {
     // TODO keep reading the socket for a relevent ip
     // update the sequence number
+/*
+    while (1) {
+    // TODO TECHNICALLY THE MAX PACKET SIZE FOR TCP IS 65535
+    char buff[MSS];
+    if (recvfrom(sock, buff, MSS, 0, NULL, NULL) < 0) {
+      perror("Error on recv()");
+    } else {
+      // TODO WE SHOULD PARSE THE RESPONSE HERE
+      struct sockaddr_in source_socket_address, dest_socket_address;
+      struct iphdr *ip_packet = (struct iphdr *) buff;
+      memset(&source_socket_address, 0, sizeof(source_socket_address));
+      source_socket_address.sin_addr.s_addr = ip_packet->saddr;
+      memset(&dest_socket_address, 0, sizeof(dest_socket_address));
+      dest_socket_address.sin_addr.s_addr = ip_packet->daddr;
+
+      if (source_socket_address.sin_addr.s_addr == dst_addr) {
+       
+        printf("Incoming packet: \n");
+        printf("Packet size (bytes) %d\n", ntohs(ip_packet->tot_len));
+        printf("Source Address: %s\n", (char *)inet_ntoa(source_socket_address.sin_addr));
+        printf("Destination Address: %s\n", (char *)inet_ntoa(dest_socket_address.sin_addr));
+        printf("Identification: %d\n\n", ntohs(ip_packet->id)); 
+        break;
+      } else {
+        printf("Does not match\n");
+      }
+      
+    }
+    }
+*/
+    return 10;
 }
 
 void handshake(struct victim_connection *m_victim, int sock) {
@@ -156,140 +266,8 @@ int beginAttack() {
 }
 
 int main(int argc, char const *argv[]) {
+  printf("In Main: About to begin:");
+  beginAttack();
 
-  if (argc != 3) {
-    printf("Error: Invalid parameters! \n");
-    printf("Usage: %s <target hostname/IP> <target port>\n", argv[0]);
-    exit(1);
-  }
-
-  u_int16_t src_port, dst_port;
-  u_int32_t src_addr, dst_addr;
-  dst_addr = inet_addr(argv[1]);
-  dst_port = atoi(argv[2]);
-
-  // TODO THIS SHOULD BE RECEIVED SOMEHOW
-  src_addr = inet_addr("10.0.0.1");
-
-  int sock, bytes_sent;
-  struct tcphdr *tcpHdr;
-
-  //TODO change this Initial guess for the SEQ field of the TCP header
-  uint32_t initSeqGuess = 1138083240;
-
-  //Data to be appended at the end of the tcp header
-  char *data;
-
-  //TCP header + data
-  char packet[MSS];
-
-  //Address struct to sendto()
-  struct sockaddr_in addr_in;
-
-  //Pseudo TCP header to calculate the TCP header's checksum
-  struct pseudoTCPPacket pTCPPacket;
-
-  //Pseudo TCP Header + TCP Header + data
-  char *pseudo_packet;
-  
-  //Raw socket without the tcp headers
-  if((sock = socket(PF_INET, SOCK_RAW, IPPROTO_TCP)) < 0) {
-    perror("Error while creating socket");
-    exit(-1);
-  }
-
-  //Populate address struct
-  addr_in.sin_family = AF_INET;
-  addr_in.sin_port = htons(dst_port);
-  addr_in.sin_addr.s_addr = dst_addr;
-
-  //Allocate mem for tcp headers and 0
-  memset(packet, 0, MSS);
-
-  tcpHdr = (struct tcphdr *) packet;
-  data = (char *) (packet + sizeof(struct tcphdr));
-  strcpy(data, DATA);
-
-  //Populate tcpHdr
-  tcpHdr->source = htons(src_port); //16 bit in nbp format of source port
-  tcpHdr->dest = htons(dst_port); //16 bit in nbp format of destination port
-  tcpHdr->seq = 0x0; //32 bit sequence number, initially set to zero
-  tcpHdr->ack_seq = 0x0; //32 bit ack sequence number, depends whether ACK is set or not
-  tcpHdr->doff = 5; //4 bits: 5 x 32-bit words on tcp header
-  tcpHdr->res1 = 0; //4 bits: Not used
-  tcpHdr->cwr = 0; //Congestion control mechanism
-  tcpHdr->ece = 0; //Congestion control mechanism
-  tcpHdr->urg = 0; //Urgent flag
-  tcpHdr->ack = 0; //Acknownledge
-  tcpHdr->psh = 0; //Push data immediately
-  tcpHdr->rst = 0; //RST flag
-  tcpHdr->syn = 1; //SYN flag
-  tcpHdr->fin = 0; //Terminates the connection
-  tcpHdr->window = htons(155);//0xFFFF; //16 bit max number of databytes 
-  tcpHdr->check = 0; //16 bit check sum. Can't calculate at this point
-  tcpHdr->urg_ptr = 0; //16 bit indicate the urgent data. Only if URG flag is set
-
-  //Now we can calculate the checksum for the TCP header
-  pTCPPacket.srcAddr = src_addr; //32 bit format of source address
-  pTCPPacket.dstAddr = dst_addr; //32 bit format of source address
-  pTCPPacket.zero = 0; //8 bit always zero
-  pTCPPacket.protocol = IPPROTO_TCP; //8 bit TCP protocol
-  pTCPPacket.TCP_len = htons(sizeof(struct tcphdr) + strlen(data)); // 16 bit length of TCP header
-
-  //Populate the pseudo packet
-  pseudo_packet = (char *) malloc((int) (sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) + strlen(data)));
-  memset(pseudo_packet, 0, sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) + strlen(data));
-
-  //Copy pseudo header
-  memcpy(pseudo_packet, (char *) &pTCPPacket, sizeof(struct pseudoTCPPacket));
- 
-  //Calculate check sum: zero current check, copy TCP header + data to pseudo TCP packet, update check
-  tcpHdr->check = 0;
-
-  //Copy tcp header + data to fake TCP header for checksum
-  memcpy(pseudo_packet + sizeof(struct pseudoTCPPacket), tcpHdr, sizeof(struct tcphdr) + strlen(data));
-
-  //Set the TCP header's check field
-  tcpHdr->check = (csum((unsigned short *) pseudo_packet, (int) (sizeof(struct pseudoTCPPacket) + sizeof(struct tcphdr) +  strlen(data))));
-
-  printf("TCP Checksum: %d\n", (int) tcpHdr->check);
-
-  //Finally, send packet
-  if((bytes_sent = sendto(sock, packet, MSS, 0, (struct sockaddr *) &addr_in, sizeof(addr_in))) < 0) {
-    perror("Error on sendto()");
-  }
-  else {
-    printf("Success! Sent %d bytes.\n", bytes_sent);
-  }
-
-  while (1) {
-    // TODO TECHNICALLY THE MAX PACKET SIZE FOR TCP IS 65535
-    char buff[MSS];
-    if (recvfrom(sock, buff, MSS, 0, NULL, NULL) < 0) {
-      perror("Error on recv()");
-    } else {
-      // TODO WE SHOULD PARSE THE RESPONSE HERE
-      struct sockaddr_in source_socket_address, dest_socket_address;
-      struct iphdr *ip_packet = (struct iphdr *) buff;
-      memset(&source_socket_address, 0, sizeof(source_socket_address));
-      source_socket_address.sin_addr.s_addr = ip_packet->saddr;
-      memset(&dest_socket_address, 0, sizeof(dest_socket_address));
-      dest_socket_address.sin_addr.s_addr = ip_packet->daddr;
-
-      if (source_socket_address.sin_addr.s_addr == dst_addr) {
-       
-        printf("Incoming packet: \n");
-        printf("Packet size (bytes) %d\n", ntohs(ip_packet->tot_len));
-        printf("Source Address: %s\n", (char *)inet_ntoa(source_socket_address.sin_addr));
-        printf("Destination Address: %s\n", (char *)inet_ntoa(dest_socket_address.sin_addr));
-        printf("Identification: %d\n\n", ntohs(ip_packet->id)); 
-        break;
-      } else {
-        printf("Does not match\n");
-      }
-      
-    }
-  }
-  close(sock);
   return 0;
 }
